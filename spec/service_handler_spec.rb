a17be4df9ff854f18ceef1d4e089e242e373bb87
Dru Ibarra <Druwerd@gmail.com>
2014-12-06 22:38:41 -0800

    add ServiceHandler spec

--- spec/service_handler_spec.rb
+++ spec/service_handler_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe HttpMailer::ServiceHandler do
 
+  describe "#initialize" do
+    let(:settings){ {:host => "host.com", :api_user=> "test_api_user", :api_key => "1357924680"} }
+    let(:service_handler){ HttpMailer::ServiceHandler.new(settings) }
+
+    it "creates an instance" do
+      expect(service_handler).to respond_to(:service_api)
+      expect(service_handler).to respond_to(:service_configuration)
+    end
+  end
+
 end
